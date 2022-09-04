cbb058b83be57792973250835275551529df502f
Vitalii Bairak <vitalii_b88@yahoo.com>
2022-09-03 22:51:45 -0400

    feat(server): separate errors

---
+++ server/errors/index.js
@@ -0,0 +1,9 @@
+import CustomError from './CustomError.js';
+import BadRequestError from './BadRequestError.js';
+import AuthError from './AuthError.js';
+
+export {
+  AuthError,
+  BadRequestError,
+  CustomError,
+}
